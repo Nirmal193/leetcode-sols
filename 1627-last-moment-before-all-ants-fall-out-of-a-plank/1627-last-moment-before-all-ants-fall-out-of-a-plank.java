fec1b2583f755603bfadd37cb113e147b64bf9dd
Nirmal193 <34956168+Nirmal193@users.noreply.github.com>
2025-05-01 00:46:02 +0530

    Time: 0 ms (100%), Space: 45.2 MB (35.27%) - LeetHub

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/1627-last-moment-before-all-ants-fall-out-of-a-plank.java
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/1627-last-moment-before-all-ants-fall-out-of-a-plank.java
@@ -1,7 +1,13 @@
 class Solution {
     public int getLastMoment(int n, int[] left, int[] right) {
-        int rightMin = Arrays.stream(right).min().orElse(Integer.MAX_VALUE);
-        int leftMax = Arrays.stream(left).max().orElse(Integer.MIN_VALUE);
+        int rightMin = Integer.MAX_VALUE;
+        int leftMax = Integer.MIN_VALUE;
+        for(int i=0;i<right.length;i++){
+            rightMin = Math.min(rightMin,right[i]);
+        }
+        for(int i=0;i<left.length;i++){
+            leftMax = Math.max(leftMax,left[i]);
+        }
         int rightAns  = (rightMin <= n) ? n - rightMin : -1;
         int leftAns = (leftMax >= 0) ? leftMax : -1;
         return Math.max(leftAns,rightAns);
