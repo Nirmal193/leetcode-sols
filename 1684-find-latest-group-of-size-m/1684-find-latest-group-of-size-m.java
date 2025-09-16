30df5a92bfa9a26da17df9fb68a1072e98709b8c
Nirmal193 <34956168+Nirmal193@users.noreply.github.com>
2025-09-16 23:04:08 +0530

    Time: 5 ms (88.3%), Space: 58.3 MB (55.32%) - LeetHub

--- 1684-find-latest-group-of-size-m/1684-find-latest-group-of-size-m.java
+++ 1684-find-latest-group-of-size-m/1684-find-latest-group-of-size-m.java
@@ -1,21 +1,33 @@
 class Solution {
     public int findLatestStep(int[] arr, int m) {
-       int n = arr.length;
-        if(m == n)
-            return n;
-        
-        TreeSet<Integer> walls = new TreeSet<Integer>();
-        walls.add(0);
-        walls.add(n+1);
-        
-        for(int i=n-1;i>=0;i--){
-            int left = walls.floor(arr[i]);
-            int right = walls.ceiling(arr[i]);
-            if(arr[i]-left-1 == m || right-arr[i]-1 == m)
-                return i;
-            walls.add(arr[i]);
+         int n = arr.length;
+        int[] string = new int[n + 2];
+        //simulation + dp
+        int count = 0;
+        int step = -1;
+        for (int i = 0; i < n; ++i) {
+            int index = arr[i];
+            int left = string[index - 1];
+            int right = string[index + 1];
+            int total = 1 + left + right;
+            if (left == m) {
+                --count;
+            }
+            if (right == m) {
+                --count;
+            }
+            if (total == m) {
+                ++count;
+            }
+            if (count > 0) {
+                step = i + 1;
+            }
+            
+            //update up to 3 points
+            string[index] = total;
+            string[index - left] = total;
+            string[index + right] = total;
         }
-        
-        return -1;
+        return step;
     }
 }
